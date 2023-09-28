a03529738552de6e328e8f9d080d5148ffb03de3
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2023-09-28 12:15:48 +0200

    Update TextArea component and bump package version (#172)
    The TextArea component's CSS classes were updated to improve styling and accessibility. Remaining styles that were causing layout issues have been removed (relative flex, h-10, etc.), and added new styles (flex w-full etc.) to ensure the component scales and aligns properly within different layouts. CSS class for the disabled state was also updated to improve appearance.
    
    In the package-lock.json file, the version is updated from 0.13.0 to 0.13.1, dependencies' versions have been updated to their latest respective versions for optimal performance and security.

--- src/components/layouts/sidebar/AppHeaderNoMenu.tsx
+++ src/components/layouts/sidebar/AppHeaderNoMenu.tsx
@@ -15,7 +15,7 @@ const AppHeaderNoMenu: React.FCC = ({ children }) => {
   const auth = useAuth();
 
   return (
-    <div className="flex items-center justify-between border-b border-gray-50 py-2.5 px-3 dark:border-dark-700">
+    <div className="flex items-center justify-between border-b border-gray-50 py-2.5 px-3 dark:border-dark-800">
       <div className={'flex items-center space-x-2.5 lg:space-x-0'}>
         <div className={'flex items-center lg:hidden'}>
           <MobileNavigation />
